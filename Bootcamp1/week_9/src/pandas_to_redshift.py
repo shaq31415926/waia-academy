ea3676e4eed86b99a35afd32c3a1a5410d7bbc6b
shaq31415926 <sarah.haq@artsymail.com>
2023-04-02 23:39:27 +0200

    updated definitions

--- Bootcamp1/week_9/src/pandas_to_redshift.py
+++ Bootcamp1/week_9/src/pandas_to_redshift.py
@@ -4,42 +4,40 @@ from io import StringIO
 
 
 def connect_to_redshift(dbname, host, port, user, password):
-    """connect to redshift"""
+    """Method that connects to redshift. This gives a warning so will look for another solution"""
 
     connect = psycopg2.connect(
-            dbname=dbname, host=host, port=port, user=user, password=password
-        )
+        dbname=dbname, host=host, port=port, user=user, password=password
+    )
 
-    cursor = connect.cursor()
-    
     print("connection to redshift made")
-    
-    return connect, cursor
+
+    return connect
 
 
 def connect_to_s3(aws_access_key_id, aws_secret_access_key):
-    """connect to s3"""
-    
-    s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
-)
-    
-     return s3_client
-    
+    """Methods that connects to s3"""
 
-def df_to_s3(df, key, s3_bucket):
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+
+    return s3_client
+
+
+def df_to_s3(df, key, s3_bucket, aws_access_key_id, aws_secret_access_key):
     """Function that writes a data frame as a .csv file to an s3 bucket"""
 
-    csv_buffer = StringIO() # create buffer to temporarily store the Data Frame
+    csv_buffer = StringIO()  # create buffer to temporarily store the Data Frame
 
-    df.to_csv(csv_buffer, index=False) # code to write the data frame as csv file
-    
+    df.to_csv(csv_buffer, index=False)  # code to write the data frame as csv file
+
     s3_client = connect_to_s3(aws_access_key_id, aws_secret_access_key)
-    
-    response = s3_client.put_object(
-            Bucket=s3_bucket, Key=key, Body=csv_buffer.getvalue()
-        ) # this code writes the temp stored csv file and writes to s3
 
-    print("Dataframe is saved as CSV in S3 bucket.") # prints a statement to let us know the file has been written
+    s3_client.put_object(
+        Bucket=s3_bucket, Key=key, Body=csv_buffer.getvalue()
+    )  # this code writes the temp stored csv file and writes to s3
+
+    print(f"The transformed data is saved as CSV in the following location s3://{s3_bucket}/{key}")
