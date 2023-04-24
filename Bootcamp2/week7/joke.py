fcd374add66352ef5e76f3e268ff0cbc00518ba6
shaq31415926 <sarah.haq11@gmail.com>
2023-04-24 13:03:46 +0200

    week7 code

---
+++ Bootcamp2/week7/joke.py
@@ -0,0 +1,4 @@
+import time
+print("What is Black and White and Red All Over?")
+time.sleep(2.4)
+print("A Newspaper.")
---
+++ Bootcamp2/week7/joke_demo.py
@@ -0,0 +1,7 @@
+import time
+
+print("Question: What is black and white and read all over?")
+# this adds a time delay of 5 seconds
+time.sleep(3)
+print("Answer: A newspaper!")
+print("Ba dum tssh!")
