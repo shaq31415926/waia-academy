8760f9769de92f917588d7230f8aa78f51890c47
shaq31415926 <sarah.haq11@gmail.com>
2023-04-26 10:51:10 +0200

    code from day3 of bootcamp

---
+++ Bootcamp2/week7/day3/src/area.py
@@ -0,0 +1,8 @@
+# creating variables that calculate the area of a rectangle
+
+height = 50
+width = 70
+area = height*width
+
+print("The area is", area, "cm2")
+print(f"The area of the rectangle is {area} cm2")
