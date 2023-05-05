40a600d7ed26a8c80b77d138d18b64d4db1f4d5c
shaq31415926 <sarah.haq11@gmail.com>
2023-05-05 13:45:38 +0200

    restructured code

--- Bootcamp1/airflow/dags/pipeline.py
+++ Bootcamp1/airflow/dags/pipeline.py
@@ -10,7 +10,8 @@ from src.extract_transform_load import extract_transform_load
 dag = DAG("etl-pipeline",
           description="Extracts Transforms and Loads Data",
           schedule_interval="@daily",
-          start_date=datetime(2023,1,1), catchup=False)
+          start_date=datetime(2023,1,1),
+          catchup=False)
 
 start_dag = DummyOperator(task_id="start_dag", dag=dag)
 end_dag = DummyOperator(task_id="end_dag", dag=dag)
