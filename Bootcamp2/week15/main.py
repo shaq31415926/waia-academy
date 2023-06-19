84518f38d4a1a4e8cf44da918216cf3e14e957d3
shaq31415926 <sarah.haq11@gmail.com>
2023-06-19 13:14:29 +0200

    updated main.py with instructions

--- Bootcamp2/week15/main.py
+++ Bootcamp2/week15/main.py
@@ -17,6 +17,7 @@ password = os.getenv("password")
 aws_access_key_id = os.getenv("aws_access_key_id")
 aws_secret_access_key_id = os.getenv("aws_secret_access_key_id")
 
+# this creates a variable that tracks the time we executed the script
 start_time = datetime.now()
 
 # make a connection to redshift and extract transactional data with transformation tasks
@@ -30,7 +31,8 @@ online_trans_deduped = identify_and_remove_duplicated_data(online_trans_cleaned)
 # next step, load the deduped data to s3
 print("Loading data to s3")
 s3_bucket = "waia-data-dump"
-key = "bootcamp2/etl/XX_online_trans_cleaned.csv"
+# the key should include your initial
+key = "bootcamp2/etl/sh_online_trans_cleaned.csv"
 
 df_to_s3(online_trans_deduped,
          key,
@@ -38,5 +40,6 @@ df_to_s3(online_trans_deduped,
          aws_access_key_id,
          aws_secret_access_key_id)
 
+# this creates a variable that calculates how long it takes to run the script
 execution_time = datetime.now() - start_time
 print(f"\nTotal execution time (hh:mm:ss.ms) {execution_time}")
